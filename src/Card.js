5f7419f11c059ad775e12aefad723b4cc46755c3
DarwinsCorn <dietrevich@gmail.com>
2023-02-07 13:58:10 -0500

    created robot card components

---
+++ src/Card.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+// class Card extends React.Component {
+//     render() {
+//         const {name, email, id} = this.props;
+//         return(
+//             <div className="tc bg-light-yellow dib br3 pa3 ma2 grow bw2 shadow-5">
+//                 <img alt="omar-pic" src={`https://robohash.org/${id}?200x200`}/>
+//                 <div>
+//                     <h2>{name}</h2>
+//                     <p><email>{email}</email></p>
+//                 </div>
+//             </div>
+//         );    
+//     }
+// }
+
+
+
+const Card = ({name,email,id}) => {
+    return(
+        <div className="tc bg-light-yellow dib br3 pa3 ma2 grow bw2 shadow-5">
+                <img alt="omar-pic" src={`https://robohash.org/${id}?200x200`}/>
+                <div>
+                    <h2>{name}</h2>
+                    <p><email>{email}</email></p>
+                </div>
+            </div>
+    );
+}
+export default Card;
